364b6343ff6df66a2ff030afbacaedc963245b6d
Nuno Cortez <nucortez1@gmail.com>
2025-11-18 14:40:57 +0000

    animations content

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -4,7 +4,6 @@ import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SmoothScroll from "@/components/SmoothScroll";
 import CustomCursor from "@/components/CustomCursor";
 
 export function generateStaticParams() {
@@ -25,21 +24,19 @@ export default async function LocaleLayout({children, params}) {
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap" rel="stylesheet" />
       </head>
-      <body className="flex flex-col">
+      <body className="flex flex-col min-h-screen">
         <NextIntlClientProvider messages={messages}>
           <CustomCursor />
-          <SmoothScroll>
-            {/* Decorative border */}
-            <div className="fixed top-0 left-0 w-screen pointer-events-none z-[100]" style={{ height: '8px' }}>
-              <div className="w-full h-full bg-[#F1C424]"></div>
-            </div>
-            
-            <Header />
-            <main className="flex-grow -mt-24">
-              {children}
-            </main>
-            <Footer />
-          </SmoothScroll>
+          {/* Decorative border */}
+          <div className="fixed top-0 left-0 right-0 pointer-events-none z-[100]" style={{ height: '8px' }}>
+            <div className="w-full h-full bg-[#F1C424]"></div>
+          </div>
+          
+          <Header />
+          <main className="flex-grow -mt-24">
+            {children}
+          </main>
+          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
